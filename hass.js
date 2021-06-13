baec2df4afe2eb1380ce1ca5dc682a3a7733aa68
ravikumar <59856007+ravikumar9130@users.noreply.github.com>
2021-06-13 11:51:20 +0530

    Update hass.js

--- hass.js
+++ hass.js
@@ -1,3 +1,25 @@
+// # question- 1
+// Looping a triangle
+// Write a loop that makes seven calls to console.log to output the following
+// triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+// 37
+// It may be useful to know that you can find the length of a string by writing
+// .length after it.
+// let abc = "abc";
+// console.log(abc.length);
+// // → 3
+
+
+
+
+
 for (let hash = "#";hash.length < 8; hash += '#'){
 
     console.log(hash )
